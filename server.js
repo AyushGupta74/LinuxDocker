8982a7fe2cd426a0feea6a7029178a39e206400b
Ayush Gupta <ayush2362000@gmail.com>
2023-10-01 21:39:18 +0530

    added files

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get("/", (req, res) => 
+{
+    res.send ("Welcome to this page!!"); 
+});
+app.get("/product", (req, res) => 
+{
+    var p ={ "Title" : "Backpack", "Color" : "Blue", "Price": 800, "Company": "Safari"}
+    res.send(p);
+});
+app.listen(7000);
+console.log("Listening on port 7000");
